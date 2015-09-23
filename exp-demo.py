79df25f0d4dcbd59bad9046c67a3b980e5eeb903
Francisco Gutierrez <fsalvador23@gmail.com>
2015-09-23 13:39:52 -0500

    added the script

---
+++ exp-demo.py
@@ -0,0 +1,55 @@
+# exp-demo.py
+# Author: Francisco Gutiérrez
+# Date: September 20, 2015
+
+# A tiny script in Python to:
+# 1) Connect to a Meteor - MongoDB document collection
+# 2) Insert demo documents
+
+from pymongo import MongoClient
+
+# connect to the MongoDB on MongoLab
+# to learn more about MongoLab visit http://www.mongolab.com
+# replace the "" in the line below with your MongoLab connection string
+# you can also use a local MongoDB instance
+connection = MongoClient("meteorDB")
+
+# connect to the students database and the ctec121 collection
+db = connection.students.ctec121
+
+# create a dictionary to hold student documents
+
+# create dictionary
+student_record = {}
+
+# set flag variable
+flag = True
+
+# loop for data input
+while (flag):
+   # ask for input
+   student_name,student_grade = input("Enter student name and grade: ").split(',')
+   # place values in dictionary
+   student_record = {'name':student_name,'grade':student_grade}
+   # insert the record
+   db.insert(student_record)
+   # should we continue?
+   flag = input('Enter another record? ')
+   if (flag[0].upper() == 'N'):
+      flag = False
+
+# find all documents
+results = db.find()
+
+print()
+print('+-+-+-+-+-+-+-+-+-+-+-+-+-+-')
+
+# display documents from collection
+for record in results:
+# print out the document
+print(record['name'] + ',',record['grade'])
+
+print()
+
+# close the connection to MongoDB
+connection.close()
