4a3d491e35379da90f94966e9bff8a1b97f3a490
Francisco Gutierrez <fsalvador23@gmail.com>
2015-09-23 23:52:42 -0500

    Fixed script

--- exp-demo.py
+++ exp-demo.py
@@ -14,6 +14,7 @@
 from pymongo import MongoClient
 import datetime
 import time
+import random
 
 ##### How many sentiment cards?
 sc = 2
@@ -22,7 +23,7 @@ cmts = 10
 #### Probability of succesful comment
 acc  = 100
 #### Text for comments and words.
-txt = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat mas"
+txt = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma"
 # Set the mongoDB location (meteor defaults)
 client = MongoClient("localhost", 3001)
 #Set the database to "meteor"
@@ -37,7 +38,7 @@ comments = db.comments
 
 # create sentiment cards
 for i in range(sc):
-    topics.insert({ "name" : "Demo Card " + str(i) , "isActive" : 1, "creator" : "Twitter", "timestamp": datetime.datetime.utcnow(), "avgSen" : 0, "total" : 0, "bars" : { "excellent" : 0, "good" : 0, "neutral" : 0, "bad" : 0, "terrible" : 0}, "keywords" : { "Guerrero" : 0, "crimen" : 0, "ayudemos" : 0, "red" : 0, "rat" : 0 }});
+    topics.insert({ "name" : "Demo Card " + str(i) , "isActive" : 1, "creator" : "Twitter", "timestamp": datetime.datetime.utcnow(), "avgSen" : 0, "total" : 0, "bars" : { "excellent" : 0, "good" : 0, "neutral" : 0, "bad" : 0, "terrible" : 0}, "keywords" : { "lorem" : 0, "ipsum" : 0, "dolor" : 0, "amet" : 0, "sit" : 0 }});
 
 # find all the topics to get the ID.
 results = topics.find()
@@ -45,7 +46,14 @@ print('Cards created...')
 
 def performQuery():
     for record in topics.find():
-        comments.insert({"topic": record['_id'], "author": "", "posted": datetime.datetime.utcnow(), "loc": {"lng": 98.91, "lat": 110.23}, "sentiment":2, "keywords": ["bonito", "hermoso"], "text": "El cielo es bonito y muy hermoso"})
+        sr = random.randrange(-2, 3)
+        comments.insert({"topic": record['_id'], "author": "", "posted": datetime.datetime.utcnow(), "loc": {"lng": 98.91, "lat": 110.23}, "sentiment":sr, "keywords": ["bonito", "hermoso"], "text": txt})
+        topics.update({'_id':record["_id"]}, {"$set": {"total" : record["total"]+1}}, upsert=False)
+        if sr == -2: topics.update({'_id':record["_id"]}, {"$set": {"bars.terrible" :  record["bars"]["terrible"]+1}}, upsert=False)
+        if sr == -1: topics.update({'_id':record["_id"]}, {"$set": {"bars.bad" :       record["bars"]["bad"]+1}}, upsert=False)
+        if sr ==  0: topics.update({'_id':record["_id"]}, {"$set": {"bars.neutral" :   record["bars"]["neutral"]+1}}, upsert=False)
+        if sr ==  1: topics.update({'_id':record["_id"]}, {"$set": {"bars.good" :      record["bars"]["good"]+1}}, upsert=False)
+        if sr ==  2: topics.update({'_id':record["_id"]}, {"$set": {"bars.excellent" : record["bars"]["excellent"]+1}}, upsert=False)
         print('comment::' + record["name"])
     time.sleep(1)
 
